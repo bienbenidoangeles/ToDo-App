be8aba7f13bc9328c2ebcca48ef3d12851bbdff5
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-01-09 08:04:08 -0500

    cleaned out ui

---
+++ ToDo App/MyDayViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyDayViewController.swift
+//  ToDo App
+//
+//  Created by Bienbenido Angeles on 1/9/20.
+//  Copyright © 2020 Bienbenido Angeles. All rights reserved.
+//
+
+import UIKit
+
+class MyDayViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
